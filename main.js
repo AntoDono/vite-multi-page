180873fb2db09ab23f4c8e68c987689b563b74b0
Anthony Zhen <anthony20151128@gmail.com>
2021-11-10 17:20:31 -0500

    initial push

---
+++ main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Humans!</h1>
+  <a href="./freerobux.html">Click here for free robux</a>
+  <a href="./freessn.html">Click here for free SSN</a>
+  <a href="./freevbucks.html">Click here for free V-bucks</a>
+`
